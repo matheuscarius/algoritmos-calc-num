d1106a68106087967bd34027e10cef9f0361d5ca
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-09-19 12:11:59 -0300

    Correct files

--- der.m
+++ der.m
@@ -1,3 +1,7 @@
 function ret = der(f,x,h)
+#ret(f,x,h)
+#f: function handle
+#x: ponto
+#h: delta
   ret = (f(x+h*x)-f(x))/(h*x);
 endfunction
--- newtone.m
+++ newtone.m
@@ -1,11 +1,11 @@
-function ret = newtone(f,x,err)
+function ret = newtone(f,x,err,h)
 #newton(f,x,n)
 #f: function handle
 #x: chute inicial
 #err: erro
+#h: variação para derivada
   while( abs(f(x)) > err )
-    EPS=1e-9;
-    df=(f(x+EPS)-f(x))/EPS;
+    df=der(f,x,h);
     x=x-f(x)./df;
   endwhile
   ret = x;
