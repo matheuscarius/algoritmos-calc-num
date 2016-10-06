dd923c089f458fbddb8846902ead4d251a7ca1f0
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-10-06 03:13:40 -0300

    Add LU Decomposition with Pivot

--- lu_pp.m
+++ lu_pp.m
@@ -1,11 +1,28 @@
-function [L,U] = lu(a)
-#[L,U] = lu(a)
+function [L,U,P] = lu_pp(a)
+#[L,U,P] = lu_pp(a)
 #a: matriz do sistema
 #L: matriz triangular inferior
 #U: matriz triangular superior
+#P: matriz permutação
   n=length(a);
   L = eye(n);
+  P = eye(n);
   for(i=1:n) #Coluna a zerar
+    id = i;
+    for(j=i+1:n)
+      if(abs(a(j,i))>abs(a(id,i)))
+        id = j;
+      endif
+    endfor
+
+    #Trocar linhas
+    aux = a(i,:);
+    a(i,:)=a(id,:);
+    a(id,:)=aux;
+    aux = P(i,:);
+    P(i,:)=P(id,:);
+    P(id,:)=aux;
+
     for(j=i+1:n) #Linha a zerar
       m=a(j,i)/a(i,i); #Coeficiente da linha
       for(k=i:n)
