469351d178c2637868d8dfd2e34ff4b105e3876a
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-10-06 03:46:24 -0300

    Adiciona método de Gauss-Jacobi

---
+++ gauss_jacobi.m
@@ -0,0 +1,30 @@
+function x = gauss_jacobi(a,b,x=0,err=1e-6,it=1e3)
+#gauss_jacobi(a,b,x=0,err=1e-6,n=1e3)
+#a: matriz do sistema
+#b: vetor das constantes
+#x: chute inicial
+#err: erro
+#it: número de iterações
+  if(x==0)
+    x = b;
+  endif
+  n=length(b);
+  for(i=1:it) #iteração
+    ox = x;
+    maior = 0;
+    for(j=1:n) #linha a calular
+      x(j)=b(j);
+      for(k=1:n) #contribuição da coluna
+        if(k==j)
+          continue; #não se contribui
+        endif
+        x(j)-=a(j,k)*ox(k);
+      endfor
+      x(j)/=a(j,j);
+    endfor
+    maior = max(maior,abs(x(j)-ox(j)));
+    if(maior < err)
+      return;
+    endif
+  endfor
+endfunction
