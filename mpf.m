0d17791f5cb15e53237176c3b375aab0149ff445
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-10-05 22:48:48 -0300

    Change mpf

--- mpf.m
+++ mpf.m
@@ -1,10 +1,15 @@
-function ret = mpf(phi,x,n=1e3)
-#mpf(phi,x,n=1e3)
+function ret = mpf(phi,x,er=1e-6,n=1e3)
+#mpf(phi,x,er=1e-6,n=1e3)
 #phi: function handle
 #x: valor inicial
+#er: erro
 #n: número de iterações
   for i=1:n
+    last = x;
     x=phi(x);
+    if(abs(x-last)<er)
+      break;
+    endif
   endfor
   ret = x;
 endfunction
