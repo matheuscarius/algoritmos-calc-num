711baf922586c9f0ea574588792f68edc520c27e
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-12-13 12:33:05 -0200

    Adiciona Euler Aperfeicoado EDO 1a ordem

---
+++ edo/euler_ap_n.m
@@ -0,0 +1,20 @@
+function y = euler_ap_n(f,a,b,n,y0)
+#y = euler_ap_n(f,a,b,n,y0)
+#f: function handle f(x,y)
+#a: limite inferior
+#b: limite superior
+#n: numero de passos
+#y0: valor inicial
+
+h = (b-a)/n;
+y = zeros(1,n);
+w = y0;
+x = a;
+
+for i=1:n
+  w += h/2*(f(x,w)+f(x+h,w+h*f(x,w)));
+  x += h;
+  y(i) = w;
+endfor
+
+endfunction
