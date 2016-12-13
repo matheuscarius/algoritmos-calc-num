b22051fc47c59d3decf8b92605167a8d95442e7b
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-12-13 20:45:50 -0200

    Modifica Diferenças Finitas

--- edo/dif_fin.m
+++ edo/dif_fin.m
@@ -11,9 +11,9 @@ function w = dif_fin(p,q,r,a,b,n,y0,yn)
 #yn: f(b)
 
 #valores iniciais
-h = (b-a)/n;
+h = (b-a)/(n+1);
 x = a+h;
-a = b = c = d = l = u = z = w = zeros(1,n-1);
+a = b = c = d = l = u = z = w = zeros(1,n);
 
 #Montagem da matriz tridiagonal
 a(1) = -2-h*h*q(x);
@@ -21,7 +21,7 @@ b(1) = 1-p(x)*h/2;
 c(1) = 0;
 d(1) = h*h*r(x)-(1+p(x)*h/2)*y0;
 
-for i=2:n-2
+for i=2:n-1
   x += h;
   a(i) = -2-h*h*q(x);
   b(i) = 1-p(x)*h/2;
@@ -30,25 +30,25 @@ for i=2:n-2
 endfor
 
 x += h;
-a(n-1) = -2-h*h*q(x);
-b(n-1) = 0;
-c(n-1) = 1+p(x)*h/2;
-d(n-1) = h*h*r(x) - (1-p(x)*h/2)*yn;
+a(n) = -2-h*h*q(x);
+b(n) = 0;
+c(n) = 1+p(x)*h/2;
+d(n) = h*h*r(x) - (1-p(x)*h/2)*yn;
 
 #Solução da matriz tridiagonal
 l(1) = a(1);
 u(1) = b(1)/l(1);
 z(1) = d(1)/l(1);
 
-for i=2:n-1
+for i=2:n
   l(i) = a(i)-c(i)*u(i-1);
   u(i) = b(i)/l(i);
   z(i) = (d(i)-c(i)*z(i-1))/l(i);
 endfor
 
-w(n-1)=z(n-1);
+w(n)=z(n);
 
-for i=n-2:-1:1
+for i=n-1:-1:1
   w(i) = z(i)-u(i)*w(i+1);
 endfor
 
