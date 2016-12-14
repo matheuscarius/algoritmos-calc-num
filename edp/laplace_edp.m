0fc06e5d894732f04d16243bed44f78c85705041
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2016-12-14 01:37:52 -0200

    Modifica Laplace EDP

--- edp/laplace_edp.m
+++ edp/laplace_edp.m
@@ -1,19 +1,21 @@
-function u = laplace_edp(n,ta,tb,tc,td,TOL=1e-6)
+function u = laplace_edp(n,m,ta,tb,tc,td,TOL=1e-6)
 
-u = zeros(n+1,n+1);
+u = zeros(n+1,m+1);
 
 for j=2:n
+  u(j,1)=tc;
+  u(j,m+1)=td;
+endfor
+for j=2:m
   u(1,j)=ta;
   u(n+1,j)=tb;
-  u(j,1)=tc;
-  u(j,n+1)=td;
 endfor
 
 D = 1;
 while(D)
   D = 0;
   for i=2:n
-    for j=2:n
+    for j=2:m
       v = (u(i-1,j)+u(i+1,j)+u(i,j+1)+u(i,j-1))/4;
       if(abs(u(i,j)-v)>TOL) D=1; endif
       u(i,j)=v;
